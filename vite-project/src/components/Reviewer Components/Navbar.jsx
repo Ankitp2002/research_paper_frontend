383d68d151a4ad0595bb30947ee9f5c7a5de05be
_Ankitdeveloper <ankit.prajapati@scriptalldna.com>
2024-11-10 10:34:07 +0530

    notification

--- vite-project/src/components/Reviewer Components/Navbar.jsx
+++ vite-project/src/components/Reviewer Components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import notificaionIcon from "../../favIcon/notification.png";
 
 const ReviewerNavbar = () => {
+  const [notificationCount, setNotificationCount] = useState(1);
   return (
     <nav className="admin-navbar">
       <div className="navbar-title">Reviewer Dashboard</div>
@@ -20,6 +22,34 @@ const ReviewerNavbar = () => {
         </li>
         <li>
           <a href="/logout">Logout</a>
+        </li>
+        <li>
+          <a href="#" style={{ position: "relative", display: "inline-block" }}>
+            <img
+              src={notificaionIcon}
+              alt="Notification"
+              style={{
+                height: 22,
+                filter: "brightness(0) invert(1)",
+              }}
+            />
+            {notificationCount > 0 && (
+              <span
+                style={{
+                  position: "absolute",
+                  top: -5,
+                  right: -5,
+                  backgroundColor: "red",
+                  color: "white",
+                  borderRadius: "50%",
+                  padding: "2px 6px",
+                  fontSize: "10px",
+                }}
+              >
+                {notificationCount}
+              </span>
+            )}
+          </a>
         </li>
       </ul>
     </nav>
--- vite-project/src/components/User Components/Navbar.jsx
+++ vite-project/src/components/User Components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import userIcon from "../../favIcon/icons8-user-16.png";
+import notificaionIcon from "../../favIcon/notification.png";
 
 const NavbarUser = ({ searchTerm, setSearchTerm }) => {
+  const [notificationCount, setNotificationCount] = useState(1);
   return (
     <nav className="admin-navbar">
       <div className="navbar-title">User Panel</div>
@@ -45,11 +47,41 @@ const NavbarUser = ({ searchTerm, setSearchTerm }) => {
         </li>
         <li>
           <a href="/user-profile">
-            <img src={userIcon} alt="Profile" style={{ height: 30 }} />
+            <img src={userIcon} alt="Profile" style={{ height: 26 }} />
           </a>
         </li>
+
         <li>
           <a href="/logout">Logout</a>
+        </li>
+        <li>
+          <a href="#" style={{ position: "relative", display: "inline-block" }}>
+            <img
+              src={notificaionIcon}
+              alt="Notification"
+              style={{
+                height: 22,
+                marginTop: 2,
+                filter: "brightness(0) invert(1)",
+              }}
+            />
+            {notificationCount > 0 && (
+              <span
+                style={{
+                  position: "absolute",
+                  top: -5,
+                  right: -5,
+                  backgroundColor: "red",
+                  color: "white",
+                  borderRadius: "50%",
+                  padding: "2px 6px",
+                  fontSize: "10px",
+                }}
+              >
+                {notificationCount}
+              </span>
+            )}
+          </a>
         </li>
       </ul>
     </nav>
